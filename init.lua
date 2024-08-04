a2f79c3282f47634bc49255a80b53cbb966c3ff3
juancolchete <juanudkp@gmail.com>
2024-08-04 16:39:38 +0000

    chore: configure nvim tree

--- init.lua
+++ init.lua
@@ -4,8 +4,15 @@ vim.cmd [[Plug 'wakatime/vim-wakatime']]
 vim.cmd [[Plug 'kdheepak/lazygit.nvim']]
 vim.cmd [[Plug 'goolord/alpha-nvim']]
 vim.cmd [[Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}]]
+vim.cmd [[Plug 'nvim-tree/nvim-tree.lua']]
 vim.cmd [[call plug#end()]]
+
+vim.g.mapleader = " "
+vim.keymap.set('n','<leader>e','<cmd>NvimTreeToggle<CR>')
+
 
 local alpha = require'alpha'
 local dashboard = require'alpha.themes.dashboard'
 alpha.setup(dashboard.config)
+local nvimTree = require'nvim-tree'
+nvimTree.setup()
