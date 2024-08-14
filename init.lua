e2bc9b18a1722d4c8baa5625e470e65898a5fad3
juancolchete <juanudkp@gmail.com>
2024-08-14 10:43:47 +0000

    chore: add lualine plugin

--- init.lua
+++ init.lua
@@ -5,6 +5,8 @@ vim.cmd [[Plug 'kdheepak/lazygit.nvim']]
 vim.cmd [[Plug 'goolord/alpha-nvim']]
 vim.cmd [[Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}]]
 vim.cmd [[Plug 'nvim-tree/nvim-tree.lua']]
+vim.cmd [[Plug 'nvim-lualine/lualine.nvim']]
+vim.cmd [[Plug 'nvim-tree/nvim-web-devicons']]
 vim.cmd [[call plug#end()]]
 
 vim.g.mapleader = " "
@@ -15,4 +17,44 @@ local alpha = require'alpha'
 local dashboard = require'alpha.themes.dashboard'
 alpha.setup(dashboard.config)
 local nvimTree = require'nvim-tree'
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
 nvimTree.setup()
